f2856b9bcc834386543486e1856416be29d6e5a5
AugustoL <augusto.lemble@gmail.com>
2018-05-26 18:31:32 -0300

    Update oracle regsitry to map by oracle types

--- contracts/OracleRegistry.sol
+++ contracts/OracleRegistry.sol
@@ -9,39 +9,45 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 contract OracleRegistry is Ownable {
 
   /// @dev Event for logging the addition of an oracle to the registry
-  /// @param oracleAddress The address of the oracle contract
+  /// @param type The type of the oracle
+  /// @param address The address of the oracle contract
   /// @param owner The owner of the oracle contract
   /// @param dataType The data type of the oracle data feed
   /// @param description The description of the oracle
-  event OracleAdded(address oracleAddress, address owner, string dataType, string description);
+  event OracleAdded(string oracleType, address addr, address owner, string dataType, string description);
 
   /// @dev Event for logging the removal of an oracle from the registry
-  /// @param oracleAddress The address of the oracle contract
-  /// @param owner The owner of the oracle contract
-  /// @param dataType The data type of the oracle data feed
-  /// @param description The description of the oracle
-  event OracleRemoved(address oracleAddress, address owner, string dataType, string description);
+  /// @param type The type of the oracle
+  /// @param address The address of the oracle contract
+  event OracleRemoved(string oracleType, address addr);
 
   /// @dev Event for logging the update of an oracle description
-  /// @param oracleAddress The address of the oracle contract
-  /// @param owner The owner of the oracle contract
-  /// @param dataType The data type of the oracle data feed
+  /// @param type The type of the oracle
+  /// @param address The address of the oracle contract
   /// @param description The description of the oracle
-  event OracleDescriptionUpdated(address oracleAddress, address owner, string dataType, string description);
-
-  // Array of oracle addresses
-  address[] public oracleAddresses;
+  event OracleDescriptionUpdated(string oracleType, address addr, string description);
 
   // Struct of information about oracle
   struct Oracle {
-    uint256 oracleAddressesIndex;
+    uint256 listIndex;
     address owner;
     string dataType;
     string description;
   }
 
-  // Map of oracle addresses to oracle structs
-  mapping(address => Oracle) oracles;
+  // Struct of information about oracle
+  struct OracleType {
+    // Array of oracle addresses
+    address[] list;
+    // Map of oracle addresses to oracle structs
+    mapping(address => Oracle) index;
+  }
+
+  // Map of oracle by types
+  mapping(address => string) oracles;
+
+  // Mapping of oracle types
+  mapping(string => OracleType) oracleTypes;
 
   /// @dev Constructor for setting up OracleRegistry
   function OracleRegistry() {
@@ -49,45 +55,56 @@ contract OracleRegistry is Ownable {
   }
 
   /// @dev Function for adding an oracle to the registry
+  /// @param _type Oracle type to be added
   /// @param _address Address of the oracle contract
   /// @param _owner Address of the owner of the oracle contract
   /// @param _dataType Type of data of the Oracle of the data feed
   /// @param _description Type of data of the Oracle of the data feed
-  function addOracle(address _address, address _owner, string _dataType, string _description) onlyOwner public {
+  function addOracle(string _type, address _address, address _owner, string _dataType, string _description) onlyOwner public {
     // get index of next address being added (same as length before oracle is added)
-    uint256 _index = oracleAddresses.length;
+    uint256 _index = oracleTypes[_type].list.length;
 
-    oracleAddresses.push(_address);
+    oracleTypes[_type].list.push(_address);
 
-    oracles[_address] = Oracle(_index, _owner, _dataType, _description);
+    oracleTypes[_type].index[_address] = Oracle(_index, _owner, _dataType, _description);
 
-    emit OracleAdded(_address, _owner, _dataType, _description);
+    emit OracleAdded(_type, _address, _owner, _dataType, _description);
   }
-	
+
   /// @dev Function for removing an oracle from the registry
   /// @param _address Address of the oracle contract to remove
   function removeOracle(address _address) onlyOwner public {
+    // fetch oracle type
+    string _type = oracles[_address];
+
     // fetch oracle information
-    Oracle storage _oracle = oracles[_address];
+    Oracle _oracle = oracleTypes[_type].index[_address];
 
     // remove oracle from the oracle addresses array
-    delete oracleAddresses[_oracle.oracleAddressesIndex];
+    delete oracleTypes[_type].list[_oracle.listIndex];
 
     // remove oracle data struct from mapping of oracle data structs
     delete oracles[_address];
+
+    emit OracleRemoved(_type, _address);
   }
 
   /// @dev Function for updating an oracel description
   /// @param _address Address of the oracle being updated
   /// @param _description New description for the oracle
   function updateOracleDescription(address _address, string _description) public {
+    // fetch oracle type
+    string _type = oracles[_address];
+
     // fetch oracle information
-    Oracle storage _oracle = oracles[_address];
+    Oracle _oracle = oracleTypes[_type].index[_address];
 
     // require caller of function to be the owner of the oracle
-    require(_oracle.owner == msg.sender);	
+    require(_oracle.owner == msg.sender);
 
     // update description
     _oracle.description = _description;
+
+    emit OracleDescriptionUpdated(_type, _address, _description);
   }
 }
