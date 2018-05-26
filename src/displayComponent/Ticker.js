478c8aba47770c63c5df10903f6fa12097c627cb
Aaron Anderson <aaronlynanderson@gmail.com>
2018-05-26 09:56:44 -0300

    displays ticker on homepage

--- src/displayComponent/Ticker.js
+++ src/displayComponent/Ticker.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react'
 //import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
-import getWeb3 from './utils/getWeb3'
+import getWeb3 from '../utils/getWeb3'
 
-import Ticker from './displayComponent/Ticker.js'
+import '../css/oswald.css'
+import '../css/open-sans.css'
+import '../css/pure-min.css'
+import './Ticker.css'
 
-import './css/oswald.css'
-import './css/open-sans.css'
-import './css/pure-min.css'
-import './App.css'
-
-class App extends Component {
+class Ticker extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      storageValue: 0,
+      data: '',
       web3: null
     }
   }
@@ -71,27 +69,18 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
-        </nav>
+      <div className="Ticker">
 
         <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+          <div className="">
+            <div className="">
+              <p>the data returned is: {this.state.data}</p>
             </div>
           </div>
         </main>
-        <Ticker />
       </div>
     );
   }
 }
 
-export default App
+export default Ticker
