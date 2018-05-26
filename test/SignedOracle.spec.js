c61cb713ed49cd1dd73ef66d02f59d7f06d26c3a
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-05-26 17:25:02 -0300

    test(signedOracle): add edit tests

--- test/SignedOracle.spec.js
+++ test/SignedOracle.spec.js
@@ -4,7 +4,6 @@ const Web3 = require('web3');
 const web3 = new Web3('http://localhost:8545');
 
 contract( 'SignedOracle', function (accounts) {
-
   let signedOracle;
   let owner = accounts[0];
   let user = accounts[1];
@@ -12,7 +11,7 @@ contract( 'SignedOracle', function (accounts) {
   let signature;
 
   const REWARD = web3.utils.toWei('0.01', 'ether')
-  const TIME_DELAY_ALLOWED = 3600 // 1 hour
+  const TIME_DELAY_ALLOWED = '3600' // 1 hour
   const DATA_TYPE = 'uint';
 
   const DATA = web3.utils.toHex(120);
@@ -78,4 +77,35 @@ contract( 'SignedOracle', function (accounts) {
     });
     expect(await web3.eth.getBalance(user)).gt(userBalance);
   })
+
+  it('Edit. Expect ok', async () => {
+    const NEW_REWARD =  web3.utils.toWei('0.1', 'ether');
+    const NEW_TIME_ALLOWED = '5000';
+    const result = await signedOracle.edit(NEW_REWARD, NEW_TIME_ALLOWED, {from: owner});
+
+    reward = await stringify(signedOracle.reward())
+    expect(reward).to.eql(NEW_REWARD);
+
+    timeAllowed = await stringify(signedOracle.timeDelayAllowed())
+    expect(timeAllowed).to.eql(NEW_TIME_ALLOWED);
+  })
+
+  it('Edit with attacker. Expect ok', async () => {
+    const NEW_REWARD =  web3.utils.toWei('0.1', 'ether');
+    const NEW_TIME_ALLOWED = 5000;
+    try {
+      const result = await signedOracle.edit(NEW_REWARD, NEW_TIME_ALLOWED, {from: attacker});
+    } catch (e ) {
+      reward = await stringify(signedOracle.reward())
+      expect(reward).to.eql(REWARD);
+
+      timeAllowed = await stringify(signedOracle.timeDelayAllowed())
+      expect(timeAllowed).to.eql(TIME_DELAY_ALLOWED);
+    }
+  })
 })
+
+const stringify = async (value) => {
+  value = await value
+  return value.toString()
+}
