1aefe1f8ce8a3a4092c362ba95191fdf5c1d135e
AugustoL <augusto.lemble@gmail.com>
2018-05-26 10:11:43 -0300

    Update interfaces

--- contracts/interfaces/IDataFeedOracle.sol
+++ contracts/interfaces/IDataFeedOracle.sol
@@ -1,5 +1,9 @@
 pragma solidity ^0.4.24;
 
+/**
+ * @title IDataFeedOracle
+ * @dev An oracle that will feed some data outside of the network
+ */
 contract IDataFeedOracle {
   function getDataType() public view returns (string);
   function getLastTimestamp() public view returns (uint256);
--- contracts/interfaces/ISignedOracle.sol
+++ contracts/interfaces/ISignedOracle.sol
@@ -2,9 +2,14 @@ pragma solidity 0.4.24;
 
 import './IDataFeedOracle';
 
+/**
+ * @title ISignedOracle
+ * @dev Signed oracle interface, an oracle that will require the a signature of the owner to be updated.
+ */
 contract ISignedOracle is IDataFeedOracle {
-  function update(bytes value, uint256 startTimestamp, uint256 endTimestamp, bytes _signature) public;
-  function edit(uint256 _reward, uint256 _timeDelay) onlyOwner;
+  function update(bytes data, uint256 dataTimestamp, bytes signature) public;
+  function edit(uint256 reward, uint256 timeDelayAllowed) onlyOwner;
+  function transferOwnership(address newOwner) onlyOwner;
   function getOwner() public view returns (address);
   function reward() public view returns (uint256);
   function timeDelayAllowed() public view returns (uint256);
