dd140744e54b8fb9f88bf15fdccbd80a867d909f
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-05-26 14:09:27 -0300

    fix migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var SignedOracle = artifacts.require("./SignedOracle.sol");
+
+
+const web3 = require('web3')
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  const REWARD = web3.utils.toWei('1', 'ether')
+  const TIME_DELAY_ALLOWED =web3.utils.toBN(3600); // 1 hour
+  const DATA_TYPE = 'int';
+  deployer.deploy(SignedOracle, REWARD, TIME_DELAY_ALLOWED, DATA_TYPE );
 };
