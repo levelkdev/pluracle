efad1d7b6980b84c97a791b32f27fdb282c6b804
Chris Whinfrey <cwhinfrey@gmail.com>
2018-05-26 11:49:46 -0300

    implement IDataFeedOracle in centralized oracle

--- contracts/CentralizedOracle.sol
+++ contracts/CentralizedOracle.sol
@@ -8,5 +8,25 @@ import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
  * @dev An oracle whose value is set by its owner
  */
 contract CentralizedOracle is IDataFeedOracle, Ownable {
-  
+
+  string _dataType;
+  uint256 _lastTimestamp;
+  bytes _data;
+
+  function CentralizedOracle(string dataType) {
+    _dataType = dataType;
+  }
+
+  function dataType() public view returns (string) {
+    return _dataType;
+  }
+
+  function lastTimestamp() public view returns (uint256) {
+    return _lastTimestamp;
+  }
+
+  function data() public view returns (bytes) {
+    return _data;
+  }
+
 }
