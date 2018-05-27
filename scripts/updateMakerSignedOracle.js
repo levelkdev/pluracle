622ab2379b0e830b843fd4d97ddbc9772c4252aa
Chris Whinfrey <cwhinfrey@gmail.com>
2018-05-27 05:29:08 -0300

    Update address again

--- scripts/updateMakerSignedOracle.js
+++ scripts/updateMakerSignedOracle.js
@@ -21,13 +21,13 @@ module.exports = async (callback) => {
       request('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD', { json: true }, (err, res, body) => {
         if (err) { return console.log(err) }
 
-        const makerSignedOracle = MakerSignedOracle.at("0x2e1fd419d202609166c1f5057f8c6e07efcdf373")
+        const makerSignedOracle = MakerSignedOracle.at("0xb2b27ce17a1f1f52ab8b7470577a396388e914ff")
         const now = moment().subtract(1, "minutes").unix()
         const price = res.body.USD
         let data = web3.toWei(price)
         console.log("Updating price: " + price + ", data: " + data)
         const message = otherWeb3.utils.soliditySha3({type: 'uint256', value: data}, {type: 'uint256', value: now});
-        const signedMessage = otherWeb3.eth.accounts.sign(message, "0x46a059700a7077b8c530809c85168b16d1774678d94ceedf9872365e85ca5de1")//0x0//web3.eth.sign(message, accounts[0]).then(console.log);
+        const signedMessage = otherWeb3.eth.accounts.sign(message, "0x46a059700a7077b8c530809c85168b16d1774678d94ceedf9872365e85ca5de1")
         makerSignedOracle.update(data, now, signedMessage.signature).then((tx) => {
           console.log("tx: " + tx.tx)
           setTimeout(updatePriceOracle, 60000);
