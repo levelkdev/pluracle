f5471d396cc1fa06ca40425b648d663714acb426
AugustoL <augusto.lemble@gmail.com>
2018-05-26 23:57:22 -0300

    Fix centralized oracle tests

--- test/CentralizedOracle.spec.js
+++ test/CentralizedOracle.spec.js
@@ -6,30 +6,22 @@ require('chai')
 const Web3 = require('web3');
 const web3 = new Web3('http://localhost:8545');
 
-const ZERO = '0x0000000000000000000000000000000000000000000000000000000000000000'
-
-contract.only( 'CentralizedOracle', function ([owner, attacker]) {
+contract( 'CentralizedOracle', function ([owner, attacker]) {
   let centralizedOracle;
-  const DATA_TYPE = 'unit'
-  const DATA = '0x7800000000000000000000000000000000000000000000000000000000000000';
+  const DATA_TYPE = 'unit256'
 
   beforeEach(async function () {
     centralizedOracle = await CentralizedOracle.new(DATA_TYPE);
   });
 
   it('Update with attacker. Expect Throw', async () => {
-    centralizedOracle.update(DATA, {from: attacker})
+    centralizedOracle.update(120, {from: attacker})
       .should.be.rejectedWith(EVMRevert);
-    expect(await centralizedOracle.data()).to.eql(ZERO);
+    expect(parseInt(await centralizedOracle.data())).to.eql(0);
   })
   it('Update with attacker. Expect ok', async () => {
-    centralizedOracle.update(DATA);
-    let data = await stringify(centralizedOracle.data())
-    expect(data).to.eql(DATA);
+    centralizedOracle.update(120);
+    let data = await centralizedOracle.data()
+    expect(parseInt(data)).to.eql(120);
   })
 })
-
-const stringify = async (value) => {
-  value = await value
-  return value.toString()
-}
