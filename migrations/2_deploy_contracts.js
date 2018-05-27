3869a17f0a67cb51ef6c0fa27957143fc1ff9e03
John Kelleher <johnpkelleher@gmail.com>
2018-05-26 22:49:45 -0400

    add more contracts to deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,30 @@
+const CentralizedOracleFactory = artifacts.require('CentralizedOracleFactory')
 const SignedOracleFactory = artifacts.require('SignedOracleFactory')
+const OracleRegistry = artifacts.require('OracleRegistry')
+const UintPluracle = artifacts.require('UintPluracle')
+const ComposableOracle = artifacts.require('ComposableOracle')
 
-// function createdSignedOracle(uint256 _reward, uint256 _timeDelayAllowed, string _dataType) public {
 const reward = 200
 const timeDelayAllowed = 5
 const dataType = "uint256"
+const maximumUpdateFrequency = 100
 
+// const oracles = []
+// const challengeAmounts = []
+// const challenPeriod = 500
 
 module.exports = function (deployer) {
   let configurableCoin, tokenVestingFactory
   deployer.then(() => {
+    return CentralizedOracleFactory.new(dataType)
+  }).then(() => {
     return SignedOracleFactory.new(reward, timeDelayAllowed, dataType)
+  }).then(() => {
+    return OracleRegistry.new()
+  }).then(() => {
+    return UintPluracle.new(reward, maximumUpdateFrequency)
+  }).then(() => {
+    // TODO add ComposableOracle to deploy
+    // return ComposableOracle.new(oracles, challengeAmounts, challengePeriod)
   })
 };
