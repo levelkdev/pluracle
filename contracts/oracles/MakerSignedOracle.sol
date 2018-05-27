9d9398d67b34aa05492fbaf24a8f62b306e28329
AugustoL <augusto.lemble@gmail.com>
2018-05-27 05:10:13 -0300

    Add MakerDAO tests

--- contracts/oracles/MakerSignedOracle.sol
+++ contracts/oracles/MakerSignedOracle.sol
@@ -3,11 +3,9 @@ pragma solidity 0.4.24;
 import './SignedOracle.sol';
 
 contract MakerSignedOracle is SignedOracle {
-  
-  function MakerSignedOracle(uint256 reward, uint256 timeDelayAllowed, string dataType)
-  SignedOracle(reward, timeDelayAllowed, dataType)
-    public
-  {}
+
+  function MakerSignedOracle(uint256 reward, uint256 timeDelayAllowed)
+  SignedOracle(reward, timeDelayAllowed) payable public {}
 
   function peek() external view returns (bytes32, bool) {
     return (bytes32(data()), now.sub(lastTimestamp()) <= _timeDelayAllowed);
